ae5f7da4a414b4a05dd3d07e5ca28983c09590a6
Re'em Kishnevsky <stonewow1@gmail.com>
2019-02-08 16:22:57 +0200

    Implemented the Background class in the stage module to handle tile-based background. iImproved docstrings in Game.py and stage.py. Added debug background textures to test tile-based background. Removed unused background textures and renamed existing ones.

--- Game.py
+++ Game.py
@@ -8,21 +8,23 @@ import os
 import random
 
 import sprites
-from stage import Viewport, Stage
+from stage import Viewport, Stage, Background
 from get_image import get_image
 
 
 # ----------------------------------------------------- Classes ------------------------------------------------------ #
 class Sprite(pygame.sprite.Sprite):
+    """Base class for all sprites in the game.
     """
-    Base class for all sprites in the game.
-    """
+
     def __init__(self, skins, animation_fps, x, y):
-        """
-        :param skins: The list of the sprite's skins - taken from 'sprites.py' module
-        :param animation_fps: Pace (in [frames per second]) of the  player's skin animation
-        :param x: Sprites's X-coordinate position
-        :param y: Sprite's Y-coordinate position
+        """Initialization.
+        
+        Arguments:
+            skins {list} -- the list of the sprite's skins - taken from 'sprites.py' module
+            animation_fps {int} -- pace (in frames per second) of the  player's skin animation
+            x {float|int} -- sprites's X-coordinate position
+            y {float|int} -- sprite's Y-coordinate position
         """
 
         pygame.sprite.Sprite.__init__(self)
@@ -37,30 +39,34 @@ class Sprite(pygame.sprite.Sprite):
         self.direction = 0  # Sprite's facing direction in degrees
 
     def draw(self):
-        """
-        Draws the sprite on the display according to its position and direction
+        """Draws the sprite on the display according to its position and direction.
         """
         self.skin = get_image(self.skins[self.skin_i])
         rotated = pygame.transform.rotate(self.skin, self.direction)
         rect = rotated.get_rect()
-        gameDisplay.blit(rotated, (self.x - stage.vp.x - rect.center[0], self.y - stage.vp.y - rect.center[1]))
-        # gameDisplay.blit(rotated, (self.x - rect.center[0], self.y - rect.center[1]))
+        gameDisplay.blit(rotated, (self.x - stage.vp.x -
+                                   rect.center[0], self.y - stage.vp.y - rect.center[1]))
 
 
 class Player(Sprite):
+    """A player object is a player shown on screen and is controlled by the user.
     """
-    A player object is a player shown on screen and is controlled by the user
-    """
+
     def __init__(self, skins, animation_fps=8, x=0, y=0,
                  speed_factor=1.2, keys=(pygame.K_w, pygame.K_a, pygame.K_s, pygame.K_d, pygame.K_SPACE)):
-        """
-        :param skins: The list of the sprite's skins - taken from 'sprites.py' module
-        :param x: Sprites's X-coordinate position1`
-        :param y: Sprite's Y-coordinate position
-        :param animation_fps: Pace (in frames per second) of the  player's skin animation
-        :param speed_factor: the higher the speed factor, the faster the player moves
-        :param keys: A tuple that contains the keys that update the player's actions in the following order:
-                     ([Thrust], [TurnLeft], [TurnRight], [Shoot])
+        """Initialization.
+        
+        Arguments:
+            skins {list} -- The list of the sprite's skins - taken from 'sprites.py' module
+        
+        Keyword Arguments:
+            animation_fps {int} -- Pace (in frames per second) of the  player's skin animation (default: {8})
+            x {int|float} -- sprites's x-coordinate (default: {0})
+            y {int|float} -- sprite's y-coordinate (default: {0})
+            speed_factor {int|float} -- the higher the speed factor, the faster the player moves (default: {1.2})
+            keys {tuple} -- A tuple that contains the keys that update the player's actions in the following order:
+                     ([Thrust], [TurnLeft], [TurnRight], [Shoot]) 
+                     (default: {(pygame.K_w, pygame.K_a, pygame.K_s, pygame.K_d, pygame.K_SPACE)})
         """
 
         Sprite.__init__(self, skins, animation_fps, x, y)
@@ -144,13 +150,18 @@ class Player(Sprite):
 
 # ---------------------------------------------------- Functions ----------------------------------------------------- #
 def HandleEvents():
+    """Handle program events (e.g. keypresses, closing of the window)
+    """
+
     global player
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit(); sys.exit()
+            pygame.quit()
+            sys.exit()
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_q:
-                pygame.quit(); sys.exit()
+                pygame.quit()
+                sys.exit()
             elif event.key == player.keys[0]:
                 player.Ignite()
             elif event.key == pygame.K_SPACE:
@@ -164,27 +175,15 @@ def HandleEvents():
 def gameLoop():
     global player
     global current_frame
-    global x, y
     while True:
         HandleEvents()
-        gameDisplay.fill(space)
-        x = round(player.x/1920) * 1920
-        y = round(player.y/1080) * 1080
-        
-
-        x -= stage.vp.x
-        y -= stage.vp.y
-        gameDisplay.blit(bg, (x, y))
-        gameDisplay.blit(bg, (x, y-1080))
-        gameDisplay.blit(bg, (x-1920, y))
-        gameDisplay.blit(bg, (x-1920, y-1080))
-
         stage.do()
         # Display FPS and the focused sprite's coordinates
         fps = clock.get_fps()
-        gameDisplay.blit(font.render("%.1f"%fps, True, pygame.Color('orange')),(0,0))
-        gameDisplay.blit(font.render("%.2f, %.2f"%(stage.focus.x,stage.focus.y), True, pygame.Color('white')),(0,30))
-        gameDisplay.blit(font.render("%.2f, %.2f"%(x+stage.vp.x,y+stage.vp.y), True, pygame.Color('white')),(0,60))
+        gameDisplay.blit(font.render("%.1f" % fps, True,
+                                     pygame.Color('orange')), (0, 0))
+        gameDisplay.blit(font.render("%.2f, %.2f" % (
+            stage.focus.x, stage.focus.y), True, pygame.Color('white')), (0, 30))
         # Cap FPS
         clock.tick(FPS)
         current_frame = (current_frame + 1) % 30
@@ -199,49 +198,34 @@ def main():
     global clock
     global stage
     global player, player2
-    global bg
     global current_frame
     global font
-    global x, y
 
     # Framerate variables:
     FPS = 60
     clock = pygame.time.Clock()
     current_frame = 0
 
-    # Colors:
-    white = (255, 255, 255)
-    black = (0, 0, 0)
-    red = (255, 0, 0)
-    green = (0, 255, 0)
-    blue = (0, 0, 255)
-    space = (10, 10, 10)
-
     # Initialize display:
     os.environ['SDL_VIDEO_CENTERED'] = '1'
     pygame.init()
-    # gameDisplay = pygame.display.set_mode((0,0), pygame.FULLSCREEN | pygame.HWSURFACE | pygame.DOUBLEBUF)  # Run fullscreen
-    gameDisplay = pygame.display.set_mode((0,0), pygame.NOFRAME)  # Run borderless windowed
+    # gameDisplay = pygame.display.set_mode((0, 0), pygame.FULLSCREEN | pygame.HWSURFACE | pygame.DOUBLEBUF)  # Run fullscreen
+    gameDisplay = pygame.display.set_mode((0, 0), pygame.NOFRAME)  # Run borderless windowed
     pygame.mouse.set_visible(False)
     pygame.display.set_caption('GunGame')
     font = pygame.font.Font(None, 30)
-    
-    # Initialize stage and add sprites:
-    stage = Stage(2)
-    player = stage.addSprite(Player(sprites.spaceship1, x=500, y=500))
-    player2 = stage.addSprite(Player(sprites.spaceship1, 8, y=64 ,keys=(pygame.K_UP, pygame.K_LEFT, pygame.K_DOWN, pygame.K_RIGHT, pygame.K_SPACE)))
-    for _ in range(8):
-        stage.addSprite(Player(sprites.missile2, 8, random.randint(0, stage.vp.w), random.randint(0, stage.vp.h), \
-            keys=(pygame.K_UP, pygame.K_LEFT, pygame.K_DOWN, pygame.K_RIGHT, pygame.K_SPACE)), 0)
-    stage.setFocus(player)
 
-    # Background
-    bg = get_image("textures\\background\\background_03.png", False)
-    x, y = 0, 0
+    # Initialize the stage and add sprites:
+    stage = Stage(2)  # 2 layers
+    player = stage.addSprite(Player(sprites.spaceship1, x=500, y=500))
+    player2 = stage.addSprite(Player(sprites.spaceship2, 8, y=64, keys=(pygame.K_UP, pygame.K_LEFT, pygame.K_DOWN, pygame.K_RIGHT, pygame.K_SPACE)))
+    stage.setFocus(player)
+    stage.setBackground([["textures\\background\\debug00.png","textures\\background\\debug10.png","textures\\background\\debug20.png"],
+                         ["textures\\background\\debug01.png","textures\\background\\debug11.png","textures\\background\\debug21.png"],
+                         ["textures\\background\\debug02.png","textures\\background\\debug12.png","textures\\background\\debug22.png"]])
 
     # End of initialization
     gameLoop()
-
 
 
 if __name__ == "__main__":
